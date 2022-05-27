bcab92d7d18eb9eda23006479c9138ed1c050375
hasanakbiyik <hasanakbiyik04@gmail.com>
2022-05-27 21:00:58 +0300

    AppCore

---
+++ AppCore/DataAccess/Bases/IRepositoryBase.cs
@@ -0,0 +1,22 @@
+﻿using AppCore.Records.Bases;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.DataAccess.Bases
+{
+    public interface IRepositoryBase<TEntity, TDbContext>: IDisposable where TEntity : RecordBase, new() where TDbContext : DbContext, new()
+    {
+        TDbContext DbContext { get; set; }
+        IQueryable<TEntity> Query();
+
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity, bool softDelete = false);
+        int Save();
+
+    }
+}
---
+++ AppCore/Records/Bases/RecordBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCore.Records.Bases
+{
+    public abstract class RecordBase 
+    {
+        public int Id { get; set; }
+        public string? Guid  { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public string? UpdatedBy { get; set; }
+        public bool? IsDeleted { get; set; }
+
+
+    }
+}
